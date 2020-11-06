4b86b57a3089d2e8935b3a1e6fd1d459111d04e4
jincio <j.incio@pitt.edu>
2020-11-06 16:52:19 -0500

    version with twitter link

--- ui.R
+++ ui.R
@@ -1,3 +1,4 @@
+
 ### ------------------------------------------------------------
 # shiny app para preparar mail para congresistas
 # Autor : varios
@@ -15,7 +16,12 @@ library(shinyjs)
 source("Functions.R")
 load("DataTotal.RData")
 dfin <- x
-url <- "https://twitter.com/intent/tweet?text=Hello%20world&url=https://shiny.rstudio.com/gallery/widget-gallery.html/"
+#url <- "https://twitter.com/intent/tweet?text=Esto%20Prueba&url=https://shiny.rstudio.com/gallery/widget-gallery.html/"
+url <- "https://twitter.com/intent/tweet?text=La%20Isla%20Bonita"
+url1 <- "https://twitter.com/intent/tweet?text="
+url2 <- "La Isla Bonita"
+url <- paste0(url1, gsub(pattern = "\\s", replacement = "%20", x = url2, perl = TRUE))
+
 
 gentabSalir <- function(){
   tp <- shiny::tabPanel(title = "Salir", value = "stop", icon = icon("circle-o-notch"))
@@ -23,7 +29,7 @@ gentabSalir <- function(){
 }
 url1=textOutput("tweet")
 tag_twitter <- function(){
-  t <- tags$a(href=url1, "Tweet", class="twitter-share-button")
+  t <- tags$a(href=url, "Tweet", class="twitter-share-button", id = "twitterbutton")
   includeScript("http://platform.twitter.com/widgets.js")
   return(t)
 }
@@ -39,11 +45,12 @@ sbp_input <- function(){
     shiny::htmlOutput("Nombre"),
     shiny::htmlOutput("DNI"),
     shiny::htmlOutput("Congresista"),
-    #tag_twitter(),
     #shiny::includeScript("http://platform.twitter.com/widgets.js"),
     fluidRow( 
       shiny::htmlOutput("button")
     ),
+    p("Para enviar tu tweet, primero da click a TEXTOEMAIL y luego has click ", htmlOutput("link", inline = TRUE),
+      class="alert2"),
     #shiny::htmlOutput("carta")
   )  
   return(sbp)
@@ -57,14 +64,14 @@ div_instructions <- function(){
       tags$li("Si quieres escribir a tus congresistas para evitar la vacancia marca sí en la pregunta"),
       tags$li("Preparamos un correo por tí: llena tu DNI y Nombre (no guardamos la info)"),
       tags$li("Copia el texto y envíalo desde tu correo."),
-      tags$li("Algunos congresistas tienen twitter. Para ellos te preparamos un link, cópialo y pégalo en tu browser ;)")
+      tags$li("Algunos congresistas tienen twitter. Para ellos te preparamos un tweet. Primero da click a TEXTOEMAIL y luego al link para enviar el tweet")
     ))
   return(dv)
 }
 
 div_explicacion <- function(){
   dv <- div(h5("El lunes 09 el Congreso votará la vacancia del presidente Martín Vizcarra por segunda vez. 
-            Creemos que si bien los hechos meritan una investigación y posible sanción estos no justifican la vacancia del presidente especialmente en plena pandemia y a poco meses de una nueva elección. 
+            Creemos que si bien los hechos ameritan una investigación y posible sanción estos no justifican la vacancia del presidente especialmente en plena pandemia y a poco meses de una nueva elección. 
             Con esta aplicación queremos acercar los congresistas a los ciudadanos.
             Hemos identificado que congresista obtuvo mayor votación en un distrito por lo que debería hacerte caso. Sino al menos le llenamos el buzón ;).
             Más información y nuesta metodología", a(href = "https://github.com/DecideBienpe/escribe-atu-congresista/blob/master/DecideBien.md", "aquí."))
@@ -76,7 +83,7 @@ mp_salida <- function(){
   mp <- shiny::mainPanel(
     shiny::h4("Los congresistas de tu región"),
     tableOutput("table2"),
-    shiny::h4("Tú representante(s)"),
+    shiny::h4("Más votado en tu distrito", class ="alert"),
     tableOutput("table1"),
     shiny::h4("Mensaje: copia el texto y mándalo desde tu correo"),
     tags$div(shiny::htmlOutput("encabezado2"),tags$br(),
@@ -84,15 +91,15 @@ mp_salida <- function(){
              tags$br(),shiny::htmlOutput("parrafo1"),tags$br(),
              shiny::htmlOutput("parrafo2"),tags$br(),
              shiny::htmlOutput("parrafo3"),tags$br(),
-             shiny::htmlOutput("firma"), tags$br(),
-             HTML('<hr style="color: blue;">'),
-             shiny::h4("Copia el texto, pégalo en tu browser y manda el tweet"),
-             shiny::verbatimTextOutput("tweet")#,
+             shiny::htmlOutput("firma"), tags$br()#,
+             #HTML('<hr style="color: blue;">'),
+             #shiny::h4("Copia el texto, pégalo en tu browser y manda el tweet"),
+             #shiny::verbatimTextOutput("tweet")#,
              #shiny::uiOutput("url"),
              #actionButton("twitter_share",
              #label = "Share",
              #icon = icon("twitter"),
-             #onclick = sprintf("window.open('%s')", url1))
+             #onclick = sprintf("window.open('%s')", htmlOutput("link", inline = TRUE)))
     ),
     shiny::htmlOutput("mensajeDataSave")
   )
@@ -100,8 +107,8 @@ mp_salida <- function(){
 }
 
 tp_email_congresista <- function(){
-  tp <- tabPanel(
-    "Email Congresista",
+  tp <- tabPanel("Email Congresista",
+    shinyjs::useShinyjs(), # to initialize shinyjs
     shiny::tags$head(includeScript("ganalytics.js")),
     includeCSS("styles.css"),
     h2("¡Decide bien! Escríbele a tu congresista", class = "centrado titulo"),
